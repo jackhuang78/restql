b122cf0bf31a264eb982005fbe19eb6e95fda0e3
Jack Huang <jack.huang78@gmail.com>
2017-11-10 00:13:40 -0800

    set up test database

--- src/Restql.test.js
+++ src/Restql.test.js
@@ -1,8 +1,14 @@
 const expect = require('chai').expect;
+const exec = require('promised-exec');
 const Restql = require('./Restql');
 
-describe('Restql', () => {
+describe('#Restql', () => {
+	before(async () => {
+		await exec('cat test/sakila-db/sakila-schema.sql test/sakila-db/sakila-data.sql | mysql -u root');
+	});
+
 	it('should be initialized', () => {
-		expect(new Restql()).to.be.not.null;
+
+		//expect(new Restql()).to.be.not.null;
 	});
 });
