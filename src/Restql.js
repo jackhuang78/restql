b9c418fcfa639a6b223380684a0875abb2e73027
Jack Huang <jack.huang78@gmail.com>
2017-11-13 22:15:50 -0800

    make private static method file scope

--- src/Restql.js
+++ src/Restql.js
@@ -4,6 +4,9 @@ const mysql = require('mysql');
 
 const escId = mysql.escapeId;
 const esc = mysql.escape;
+const _formWhere = (query) => Object.entries(query)
+	.map(([key, value]) => `${escId(key)}=${esc(value)}`)
+	.reduce((cond1, cond2) => `${cond1} AND ${cond2}`, 'TRUE');
 
 class Restql {
 	constructor(host, user, password, database) {
@@ -21,19 +24,13 @@ class Restql {
 		const connection = this._connect();
 		try {
 			const records = await connection.exec(
-				`SELECT * FROM ${escId(table)} WHERE ${this._formWhere(query)};`
+				`SELECT * FROM ${escId(table)} WHERE ${_formWhere(query)};`
 			);
 			return records;
 
 		} finally {
 			await connection.end();	
 		}
-	}
-
-	_formWhere(query) {
-		return Object.entries(query)
-			.map(([key, value]) => `${escId(key)}=${esc(value)}`)
-			.reduce((cond1, cond2) => `${cond1} AND ${cond2}`, 'TRUE');
 	}
 }
 
